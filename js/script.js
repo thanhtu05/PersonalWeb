c7b8b3edcbe92c8377f2abb721670e987e8fc0bf
thanhtu05 <23110167@student.hcmute.edu.vn>
2025-12-03 22:53:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Đây là nơi bạn sẽ thêm mã JavaScript tương tác sau này.
+// Hiện tại, nó chỉ là một file trống để thiết lập cấu trúc.
+
+console.log("Website giới thiệu cá nhân đã tải thành công!");
+// Ví dụ: thêm logic cho menu responsive
+// document.querySelector('.logo').addEventListener('click', () => { ... });
